973fa52365a18b67069ca397dc1db81c013320f8
Dakshita Jena <djena1399@gmail.com>
2020-12-29 21:29:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Lifemembership from './Lifemembership/lifemembership';
+import Search from './Lifemembership/Search';
+import Footer from "./Lifemembership/Footer";
+function App() {
+  return (
+    <div className="App">
+      <Lifemembership/>;
+      <Search/>;
+      <Footer/>;
+    </div>
+  );
+}
+
+export default App;
